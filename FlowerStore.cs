f9cc0ff7a5dc1674700f600dd70761d43507e2a8
Naruepon <78534947+NarueponL@users.noreply.github.com>
2021-10-03 01:59:17 +0700

    Midterm3

---
+++ FlowerStore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FlowerStore
+{
+    public List<string> flowerList = new List<string>();
+    List<string> cart = new List<string>();
+
+    public FlowerStore()
+    {
+        flowerList.Add("Rose");
+        flowerList.Add("Lotus");
+    }
+
+    public void AddFlowerToCart(string name) //เพิ่มดอกไม้ในตระกร้าของเรา
+    {
+        cart.Add(name);
+    }
+
+    public void showCart() //แสดงหน้าดอกไม้ที่ซื้อ
+    {
+        if (cart.Count == 0)
+        {
+            Console.WriteLine("Cart is empty");
+        }
+        else
+        {
+            Console.WriteLine("My Cart :");
+            foreach (string i in cart)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Midterm3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlowerStore flowerStore = new FlowerStore();
+
+            BuyFlowerPage(flowerStore);
+            ShowCart(flowerStore);
+
+        }
+
+        static void BuyFlowerPage(FlowerStore flowerStore) //แสดงหน้าเลือกดอกไม้
+        {
+            SelectFlowerHeader();
+            AddFlower(flowerStore);
+            StopProgess(flowerStore);
+        }
+
+        static void SelectFlowerHeader() //แสดงประเภทดอกไม้ที่ให้ซื้อ
+        {
+            Console.WriteLine("Select number for buy flower :");
+            Console.WriteLine("1. Rose");
+            Console.WriteLine("2. Lotus");
+        }
+
+        static int InputFlowerSelect() //เลือกดอกไม้
+        {
+            Console.Write("Input: ");
+            return int.Parse(Console.ReadLine());            
+        }
+        static void AddFlower(FlowerStore flowerStore) //เพิ่มดอกไม้ในList
+        {
+            switch (InputFlowerSelect())
+            {
+                case 1:
+                    flowerStore.AddFlowerToCart(flowerStore.flowerList[0]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[0]);
+                    break;
+                case 2:
+                    flowerStore.AddFlowerToCart(flowerStore.flowerList[1]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[1]);
+                    break;
+                default:
+                    Console.WriteLine("Not Added to cart. found select number of flower");
+                    break;
+            }
+        }
+        static void StopProgess(FlowerStore flowerStore) //หยุดซื้อดอกไม้เพิ่ม
+        {
+            string decide = "";
+            
+            Console.WriteLine("You can stop this progress ? exit for >> exit << progress and pressany key for continue");
+            decide = Console.ReadLine();
+
+            if (decide != "exit")
+            {
+                BuyFlowerPage(flowerStore);
+            }
+        }
+        static void ShowCart(FlowerStore flowerStore) //แสดงดอกไม้ที่ซื้อ
+        {
+            Console.WriteLine("Current my cart");
+            flowerStore.showCart();
+        }
+    }
+
+}
